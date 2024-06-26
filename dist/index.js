da6849fca48b964e504189efda4b712d42a6d052
Yiminju <yiminju0910@gmail.com>
2024-06-27 02:58:22 +0900

    왜 커밋이 129개나 되는걸까?

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//string
+let greeting = "Hello, world!";
+console.log(greeting);
+//number
+let age = 30;
+console.log(age);
+//boolean
+let isStudent = true;
+console.log(isStudent);
+//null
+let middleName = null;
+console.log(middleName);
+//any
+let dynamicValue = "Hello, world!";
+console.log(dynamicValue);
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+//string
+let greeting: string = "Hello, world!";
+console.log(greeting);
+
+//number
+let age: number = 30;
+console.log(age);
+
+//boolean
+let isStudent: boolean = true;
+console.log(isStudent);
+
+//null
+let middleName: string | null = null;
+console.log(middleName);
+
+//any
+let dynamicValue: any = "Hello, world!";
+console.log(dynamicValue);
